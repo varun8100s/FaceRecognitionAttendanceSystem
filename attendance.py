0a38dd9f3b292096c60b0fedede419b45d109465
varun8100s <varunraveesh@gmail.com>
2022-05-29 23:24:49 +0530

    initial commit

---
+++ attendance.py
@@ -0,0 +1,124 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+from tkinter import *
+import tkinter.ttk as ttk
+import csv
+
+
+
+path = 'images'
+images = []
+personNames = []
+myList = os.listdir(path)
+for cu_img in myList:
+    current_Img = cv2.imread(f'{path}/{cu_img}')
+    images.append(current_Img)
+    personNames.append(os.path.splitext(cu_img)[0])
+print(personNames)
+
+
+def faceEncodings(images):
+    print("Encoding....")
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def attendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            time_now = datetime.now()
+            tStr = time_now.strftime('%H:%M:%S')
+            dStr = time_now.strftime('%d/%m/%Y')
+            f.writelines(f'\n{name},{tStr},{dStr}')
+
+
+encodeListKnown = faceEncodings(images)
+print('Encoding Successful!')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    faces = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+    faces = cv2.cvtColor(faces, cv2.COLOR_BGR2RGB)
+
+    facesCurrentFrame = face_recognition.face_locations(faces)
+    encodesCurrentFrame = face_recognition.face_encodings(faces, facesCurrentFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = personNames[matchIndex].upper()
+            # print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            attendance(name)
+
+    cv2.imshow('Webcam', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+root = Tk()
+root.title("Attendance List")
+width = 500
+height = 400
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+root.resizable(0, 0)
+
+
+TableMargin = Frame(root, width=500)
+TableMargin.pack(side=TOP)
+scrollbarx = Scrollbar(TableMargin, orient=HORIZONTAL)
+scrollbary = Scrollbar(TableMargin, orient=VERTICAL)
+tree = ttk.Treeview(TableMargin, columns=("Name", "Time", "Date"), height=400, selectmode="extended", yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
+scrollbary.config(command=tree.yview)
+scrollbary.pack(side=RIGHT, fill=Y)
+scrollbarx.config(command=tree.xview)
+scrollbarx.pack(side=BOTTOM, fill=X)
+tree.heading('Name', text="Name", anchor=W)
+tree.heading('Time', text="Time", anchor=W)
+tree.heading('Date', text="Date", anchor=W)
+tree.column('#0', stretch=NO, minwidth=0, width=0)
+tree.column('#1', stretch=NO, minwidth=0, width=200)
+tree.column('#2', stretch=NO, minwidth=0, width=200)
+tree.column('#3', stretch=NO, minwidth=0, width=300)
+tree.pack()
+
+with open('attendance.csv') as f:
+    reader = csv.DictReader(f, delimiter=',')
+    for row in reader:
+        Name = row['Name']
+        Time = row['Time']
+        Date = row['Date']
+        tree.insert("", 0, values=(Name, Time, Date))
+
+#============================INITIALIZATION==============================
+if __name__ == '__main__':
+    root.mainloop()
+
+cap.release()
+cv2.destroyAllWindows()
